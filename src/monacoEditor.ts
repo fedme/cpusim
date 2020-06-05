e9d40e203eab1888f50e245006d6ed9321b73a3a
fedme <federico.meini@gmail.com>
2020-06-05 15:57:41 +0200

    Add autocomplete

--- src/monacoEditor.ts
+++ src/monacoEditor.ts
@@ -3,6 +3,41 @@ import { SyntaxError } from './parser'
 
 export type MonacoEditor = any
 
+enum CompletionItemKind {
+  Method = 0,
+  Function = 1,
+  Constructor = 2,
+  Field = 3,
+  Variable = 4,
+  Class = 5,
+  Struct = 6,
+  Interface = 7,
+  Module = 8,
+  Property = 9,
+  Event = 10,
+  Operator = 11,
+  Unit = 12,
+  Value = 13,
+  Constant = 14,
+  Enum = 15,
+  EnumMember = 16,
+  Keyword = 17,
+  Text = 18,
+  Color = 19,
+  File = 20,
+  Reference = 21,
+  Customcolor = 22,
+  Folder = 23,
+  TypeParameter = 24,
+  Snippet = 25
+}
+
+enum CompletionItemInsertTextRule {
+  KeepWhitespace = 1,
+  InsertAsSnippet = 4
+}
+
+
 export function configureMonacoEditor(monacoInstance: MonacoEditor) {
   // Register a new language
   monacoInstance.languages.register({ id: 'cpusim' })
@@ -30,6 +65,143 @@ export function configureMonacoEditor(monacoInstance: MonacoEditor) {
       { token: 'spAddress', foreground: 'F92672' }
     ],
     colors: {
+    }
+  })
+
+  // Register a completion item provider for the new language
+  monacoInstance.languages.registerCompletionItemProvider('cpusim', {
+    provideCompletionItems: () => {
+      const suggestions = [
+        {
+          label: 'R0',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'R0'
+        },
+        {
+          label: 'R1',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'R1'
+        },
+        {
+          label: 'IX',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'IX'
+        },
+        {
+          label: 'SP',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'SP'
+        },
+        {
+          label: 'NOP',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'NOP'
+        },
+        {
+          label: 'HLT',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'HLT'
+        },
+        {
+          label: 'ADD',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'ADD'
+        },
+        {
+          label: 'SUB',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'SUB'
+        },
+        {
+          label: 'MUL',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'MUL'
+        },
+        {
+          label: 'DIV',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'DIV'
+        },
+        {
+          label: 'MOV',
+          kind: CompletionItemKind.Snippet,
+          // eslint-disable-next-line no-template-curly-in-string
+          insertText: 'MOV ${1:register}',
+          insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet
+        },
+        {
+          label: 'SET',
+          kind: CompletionItemKind.Snippet,
+          // eslint-disable-next-line no-template-curly-in-string
+          insertText: 'SET ${1:register} ${2:data}',
+          insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet
+        },
+        {
+          label: 'LOD',
+          kind: CompletionItemKind.Snippet,
+          // eslint-disable-next-line no-template-curly-in-string
+          insertText: 'LOD ${1:register} ${2:address}',
+          insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet
+        },
+        {
+          label: 'STO',
+          kind: CompletionItemKind.Snippet,
+          // eslint-disable-next-line no-template-curly-in-string
+          insertText: 'STO ${1:address}',
+          insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet
+        },
+        {
+          label: 'JMP',
+          kind: CompletionItemKind.Snippet,
+          // eslint-disable-next-line no-template-curly-in-string
+          insertText: 'JMP ${1:address}',
+          insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet
+        },
+        {
+          label: 'JMZ',
+          kind: CompletionItemKind.Snippet,
+          // eslint-disable-next-line no-template-curly-in-string
+          insertText: 'JMZ ${1:address}',
+          insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet
+        },
+        {
+          label: 'JML',
+          kind: CompletionItemKind.Snippet,
+          // eslint-disable-next-line no-template-curly-in-string
+          insertText: 'JML ${1:address}',
+          insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet
+        },
+        {
+          label: 'JMG',
+          kind: CompletionItemKind.Snippet,
+          // eslint-disable-next-line no-template-curly-in-string
+          insertText: 'JMG ${1:address}',
+          insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet
+        },
+        {
+          label: 'PSH',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'PSH'
+        },
+        {
+          label: 'POP',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'POP'
+        },
+        {
+          label: 'CAL',
+          kind: CompletionItemKind.Snippet,
+          // eslint-disable-next-line no-template-curly-in-string
+          insertText: 'CAL ${1:address}',
+          insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet
+        },
+        {
+          label: 'RET',
+          kind: CompletionItemKind.Keyword,
+          insertText: 'RET'
+        }
+      ]
+      return { suggestions }
     }
   })
 }
